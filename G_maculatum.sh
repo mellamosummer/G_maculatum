cec188f1ff955ec490696676b5ea746d4dd0a527
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-10-21 12:21:09 -0400

    updated multiqc outdir

--- G_maculatum.sh
+++ G_maculatum.sh
@@ -18,36 +18,37 @@ if [ ! -d $OUTDIR ]
 then
     mkdir -p $OUTDIR
 fi
-
-
-load modules
-module load FastQC/0.11.9-Java-11
+#
+#
+# load modules
+# module load FastQC/0.11.9-Java-11
 module load MultiQC/1.8-foss-2019b-Python-3.7.4
-module load Trimmomatic/0.39-Java-1.8.0_144
-module load SPAdes/3.14.1-GCC-8.3.0-Python-3.7.4
-module load QUAST/5.0.2-foss-2019b-Python-3.7.4
-module load Jellyfish/2.3.0-GCC-8.3.0
+# module load Trimmomatic/0.39-Java-1.8.0_144
+# module load SPAdes/3.14.1-GCC-8.3.0-Python-3.7.4
+# module load QUAST/5.0.2-foss-2019b-Python-3.7.4
+# module load Jellyfish/2.3.0-GCC-8.3.0
 
 # #QC pre-trim with FASTQC & MultiQC
 # mkdir $OUTDIR/FastQC
 # mkdir $OUTDIR/FastQC/pretrim
 # fastqc -o $OUTDIR/FastQC/pretrim/ /home/srb67793/G_maculatum/*.gz
-# multiqc $OUTDIR/FastQC/pretrim/*.zip
+multiqc $OUTDIR/FastQC/pretrim/*.zip -o $OUTDIR/FastQC/pretrim/
 
 #trim reads with trimmomatic
-mkdir $OUTDIR/trimmomatic
-java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE  -threads 4 \
-/home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_1.fq.gz \
-/home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_2.fq.gz \
-$OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R1_paired.fq.gz \
-$OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R1_unpaired.fq.gz \
-$OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R2_paired.fq.gz \
-$OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R2_unpaired.fq.gz  \
-ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE-2.fa:2:30:10 \
-LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+# mkdir $OUTDIR/trimmomatic
+# java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE  -threads 4 \
+# /home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_1.fq.gz \
+# /home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_2.fq.gz \
+# $OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R1_paired.fq.gz \
+# $OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R1_unpaired.fq.gz \
+# $OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R2_paired.fq.gz \
+# $OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R2_unpaired.fq.gz  \
+# ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE-2.fa:2:30:10 \
+# LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 #
 # #QC post-trim with FASTQC & MultiQC
+# mkdir $OUTDIR/FastQC/trimmed
 # fastqc -o $OUTDIR/FastQC/trimmed $OUTDIR/trimmomatic/*.gz
 # multiqc $OUTDIR/FastQC/trimmed/*.zip
 #
