2eccef8b44f7aee94b6421bf6b0c1806e15ed77c
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-10-21 10:01:05 -0400

    running fastqc

--- G_maculatum.sh
+++ G_maculatum.sh
@@ -24,29 +24,42 @@ fi
 module load FastQC/0.11.9-Java-11
 module load MultiQC/1.8-foss-2019b-Python-3.7.4
 module load Trimmomatic/0.39-Java-1.8.0_144
-module load Jellyfish/2.3.0-GCC-8.3.0
 module load SPAdes/3.14.1-GCC-8.3.0-Python-3.7.4
+module load QUAST/5.0.2-foss-2019b-Python-3.7.4
+module load Jellyfish/2.3.0-GCC-8.3.0
 
 #QC pre-trim with FASTQC & MultiQC
 fastqc -o $OUTDIR/FastQC/pretrim /home/srb67793/G_maculatum/*.gz
 multiqc $OUTDIR/FastQC/pretrim/*.zip
 
-#trim reads with trimmomatic
-java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE  -threads 4 \
-/home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_1.fq.gz /home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_2.fq.gz \
-$OUTDIR/trimmomatic/R1_paired.fastq.gz \
-$OUTDIR/trimmomatic/R1_unpaired.fastq.gz \
-$OUTDIR/trimmomatic/R2_paired.fastq.gz \
-$OUTDIR/trimmomatic/R2_unpaired.fastq.gz \
-ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE-2.fa:2:30:10 \
-LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+# #trim reads with trimmomatic
+# java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE  -threads 4 \
+# /home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_1.fq.gz /home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_2.fq.gz \
+# $OUTDIR/trimmomatic/R1_paired.fastq.gz \
+# $OUTDIR/trimmomatic/R1_unpaired.fastq.gz \
+# $OUTDIR/trimmomatic/R2_paired.fastq.gz \
+# $OUTDIR/trimmomatic/R2_unpaired.fastq.gz \
+# ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE-2.fa:2:30:10 \
+# LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+#
+# #QC post-trim with FASTQC & MultiQC
+# fastqc -o $OUTDIR/FastQC/trimmed $OUTDIR/trimmomatic/*.gz
+# multiqc $OUTDIR/FastQC/trimmed/*.zip
+#
+# #assemble the  genome using Illumina short reads with SPAdes
+# spades.py -t 6 -k 21,33,55,77 --isolate --memory 24 --pe1-1 $OUTDIR/trimmomatic/R1_paired.fastq.gz --pe1-2 /$OUTDIR/trimmomatic/R2_paired.fastq.gz -o $OUTDIR/spades
 
-#QC post-trim with FASTQC & MultiQC
-fastqc -o $OUTDIR/FastQC/trimmed $OUTDIR/trimmomatic/*.gz
-multiqc $OUTDIR/FastQC/trimmed/*.zip
+# #can i do quast or mummer?
+#
+# #kmer analysis with Jellyfish
+# jellyfish count -m 31 -s 100M -t 10 -C reads.fasta -o $OUTDIR/jellyfish
 
-#assemble the  genome using Illumina short reads with SPAdes
-spades.py -t 6 -k 21,33,55,77 --isolate --memory 24 --pe1-1 $OUTDIR/trimmomatic/R1_paired.fastq.gz --pe1-2 /$OUTDIR/trimmomatic/R2_paired.fastq.gz -o $OUTDIR/spades
+#genome scope for analysis of kmers & findGSE https://github.com/schneebergerlab/findGSE
 
-#kmer analysis with Jellyfish
-jellyfish count -m 31 -s 100M -t 10 -C reads.fasta -o $OUTDIR/jellyfish
+#kmer analysis & plastid assembly
+#spades and then BLAST or mummer or use minimap to pull out plastome
+  #to do search of similar plastid assembly
+  #fethc those pieces
+  #then use another tool for comparative scaffolding
+
+#related species -- map the reads to it
