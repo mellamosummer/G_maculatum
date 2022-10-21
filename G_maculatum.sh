a57d97b2e30ae84136dab93e725a1aa52ba865d9
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-10-21 09:42:20 -0400

    moved order of jellyfish

--- G_maculatum.sh
+++ G_maculatum.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=homework_3		                        # Job name
+#SBATCH --job-name=G_maculatum                   # Job name
 #SBATCH --partition=batch		                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
 #SBATCH --cpus-per-task=6		                            # Number of cores per task - match this to the num_threads used by BLAST
@@ -10,10 +10,43 @@
 #SBATCH --mail-type=END,FAIL                          # Mail events (BEGIN, END, FAIL, ALL)
 
 #set output directory variable
-OUTDIR="/work/jlmlab/G_maculatum
+OUTDIR="/work/jlmlab/G_maculatum"
 
 #if output directory doesn't exist, create it
+
 if [ ! -d $OUTDIR ]
 then
     mkdir -p $OUTDIR
 fi
+
+
+#load modules
+module load FastQC/0.11.9-Java-11
+module load MultiQC/1.8-foss-2019b-Python-3.7.4
+module load Trimmomatic/0.39-Java-1.8.0_144
+module load Jellyfish/2.3.0-GCC-8.3.0
+module load SPAdes/3.14.1-GCC-8.3.0-Python-3.7.4
+
+#QC pre-trim with FASTQC & MultiQC
+fastqc -o $OUTDIR/FastQC/pretrim /home/srb67793/G_maculatum/*.gz
+multiqc $OUTDIR/FastQC/pretrim/*.zip
+
+#trim reads with trimmomatic
+java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE  -threads 4 \
+/home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_1.fq.gz /home/srb67793/G_maculatum/OT1_CKDN220054653-1A_HF33VDSX5_L1_2.fq.gz \
+$OUTDIR/trimmomatic/R1_paired.fastq.gz \
+$OUTDIR/trimmomatic/R1_unpaired.fastq.gz \
+$OUTDIR/trimmomatic/R2_paired.fastq.gz \
+$OUTDIR/trimmomatic/R2_unpaired.fastq.gz \
+ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE-2.fa:2:30:10 \
+LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+#QC post-trim with FASTQC & MultiQC
+fastqc -o $OUTDIR/FastQC/trimmed $OUTDIR/trimmomatic/*.gz
+multiqc $OUTDIR/FastQC/trimmed/*.zip
+
+#assemble the  genome using Illumina short reads with SPAdes
+spades.py -t 6 -k 21,33,55,77 --isolate --memory 24 --pe1-1 $OUTDIR/trimmomatic/R1_paired.fastq.gz --pe1-2 /$OUTDIR/trimmomatic/R2_paired.fastq.gz -o $OUTDIR/spades
+
+#kmer analysis with Jellyfish
+jellyfish count -m 31 -s 100M -t 10 -C reads.fasta -o $OUTDIR/jellyfish
