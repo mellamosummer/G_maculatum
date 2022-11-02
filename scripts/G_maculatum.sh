3fa3cab4b9e0e4d261660807f8269284c8e3bcda
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-11-02 11:28:47 -0400

    update resources

--- scripts/G_maculatum.sh
+++ scripts/G_maculatum.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=highmem_p                        # Partition (queue) name
 #SBATCH --ntasks=4			                                # Single task job
 #SBATCH --cpus-per-task=8	                            # Number of cores per taskT
-#SBATCH --mem=400gb	                                # Total memory for job
-#SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --mem=500gb	                                # Total memory for job
+#SBATCH --time=160:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output="/home/srb67793/G_maculatum_novogene/log.%j"			    # Location of standard output and error log files
 #SBATCH --mail-user=srb67793@uga.edu                    # Where to send mail
 #SBATCH --mail-type=END,FAIL                          # Mail events (BEGIN, END, FAIL, ALL)
