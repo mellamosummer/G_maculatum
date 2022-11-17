cbfb056fecc582f17adaeb5904c1bb3451c53f1b
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2022-11-17 14:07:17 -0500

    mapping reads to g incanum plastome

--- scripts/G_maculatum.sh
+++ scripts/G_maculatum.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=novo37                   # Job name
 #SBATCH --partition=batch                  # Partition (queue) name
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=6
 #SBATCH --mem=100gb
 #SBATCH --time=12:00:00		                            # Time limit hrs:min:sec
 #SBATCH --output="/home/srb67793/G_maculatum_novogene/log.%j"			    # Location of standard output and error log files
@@ -61,12 +61,16 @@ OUTDIR="/scratch/srb67793/G_maculatum"
 # module load MultiQC/1.8-foss-2019b-Python-3.7.4
 # module load ml Trimmomatic/0.39-Java-1.8.0_144
 # module load GetOrganelle/1.7.5.2-foss-2020b
-module load NOVOPlasty/4.2-GCCcore-8.3.0
+# module load NOVOPlasty/4.2-GCCcore-8.3.0
 # module load BLAST+/2.9.0-gompi-2019b
 # module load QUAST/5.0.2-foss-2019b-Python-3.7.4
 # module load Jellyfish/2.3.0-GCC-8.3.0
 # module load GenomeScope/2.0-foss-2020b-R-4.2.1
 # ml Meraculous/2.2.6
+module load SRA-Toolkit/2.11.1-centos_linux64
+module load BWA/0.7.17-GCC-8.3.0
+module load SAMtools/1.10-GCC-8.3.0
+module load BCFtools/1.10.2-GCC-8.3.0
 
 ####################################################################
 # 1) TRIMS G MACULATUM ILLUMINA SHORT READS
@@ -107,8 +111,34 @@ module load NOVOPlasty/4.2-GCCcore-8.3.0
 #
 # mkdir $OUTDIR/novoplasty
 # cd $OUTDIR/novoplasty #need to be in directory for some reason
-NOVOPlasty4.2.pl -c /home/srb67793/G_maculatum_novogene/scripts/config.txt
+# NOVOPlasty4.2.pl -c /home/srb67793/G_maculatum_novogene/scripts/config.txt
 # cd /home/srb67793/G_maculatum_novogene
+
+####################################################################
+# 3) Maps trimmed reads to reference plastome
+####################################################################
+
+mkdir $OUTDIR/mapping
+
+#Constructs a BWA index for the reference plastome
+bwa index $OUTDIR/plastome/G_incanum_plastomesequence.fasta
+
+#Maps the E. coli C600 reads to the E. coli MG1655 refseq reference genome
+#stores the mapped reads in sorted .bam format
+bwa mem -t 6 $OUTDIR/plastome/G_incanum_plastomesequence.fasta $OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R1_paired.fq $OUTDIR/trimmomatic/OT1_CKDN220054653-1A_HF33VDSX5_L1_R2_paired.fq > $OUTDIR/mapping/G_maculatum.bam
+
+samtools sort $OUTDIR/mapping/G_maculatum.bam -o $OUTDIR/mapping/G_maculatum.sorted.bam
+
+samtools index -@ 6 $OUTDIR/mapping/G_maculatum.sorted.bam
+
+#Calls variants from (i) reads with mapping quality greater than 60, excludes variants that have a (ii) quality score of less than 40, and excludes variants that are (iii) supported by fewer than 10 reads for the E. coli C600 genome using `bcftools mpileup`, `bcftools call`, and `bcftools filter` (BCFtools/1.10.2-GCC-8.3.0):
+
+bcftools mpileup -Ou --threads 6 --min-MQ 60 -f $OUTDIR/plastome/G_incanum_plastomesequence.fasta \
+$OUTDIR/mapping/G_maculatum.sorted.bam | bcftools call --threads 6 -mv -Ou \
+--ploidy 1 | bcftools filter -Oz -e 'QUAL<40 || DP<10' > \
+$OUTDIR/mapping/G_maculatum.sorted.mpileup.call.filter.onestep.vcf.gz
+bcftools view $OUTDIR/mapping/G_maculatum.sorted.mpileup.call.filter.onestep.vcf.gz
+
 
 ####################################################################
 # 4) NEED TO ANNOTATE PLASTOME
